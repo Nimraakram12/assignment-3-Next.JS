8e9396ac3d36aa7cf3776b63be6e7256382182da
Nimraakram12 <160039901+Nimraakram12@users.noreply.github.com>
2024-10-30 11:57:43 -0700

    Update HeroSection.tsx

--- app/components/HeroSection.tsx
+++ app/components/HeroSection.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import styles from './HeroSection.module.css';
 
-export const HeroSection = () => {
+const HeroSection = () => {
   return (
 <section className={styles.hero}>
       <div className={styles.container}>
